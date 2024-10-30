1eef3109b8f285db55bdf29539ae69c674c41436
Shaurya Dwivedi <shauryadwivedi0108@gmail.com>
2024-10-29 23:06:39 -0500

    added escape and quit

---
+++ WinVolume.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinVolume : MonoBehaviour
+{
+    private UIController UIController;
+    [SerializeField] private string winText = "you win";
+    [SerializeField] private AudioSource winSoundPrefab;
+
+    private void Awake()
+    {
+        UIController = FindObjectOfType<UIController>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        other.gameObject.SetActive(false);
+
+        if (UIController != null) {
+            UIController.ShowWinText(winText);
+        }
+
+        if (winSoundPrefab != null)
+        {
+            SoundPlayer.Instance.PlaySFX(winSoundPrefab, transform.position);
+        }
+    }
+}
